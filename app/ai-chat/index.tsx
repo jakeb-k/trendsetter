7096554cdde9f987985fe576e77a2f2062f7974d
jakeb-k <jknow30@outlook.com>
2025-10-04 15:57:30 +1000

    [FIX] put ai chat generated goals into storage using store

--- app/ai-chat/index.tsx
+++ app/ai-chat/index.tsx
@@ -4,6 +4,7 @@ import PrimaryButton from '@/components/common/PrimaryButton';
 import TypingLoader from '@/components/common/TypingLoader';
 import { ThemedView } from '@/components/ThemedView';
 import { useEventsStore } from '@/stores/useEventStore';
+import { useGoalsStore } from '@/stores/useGoalStore';
 import Message from '@/types/models/Message';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -14,7 +15,7 @@ import {
     Text,
     TextInput,
     TouchableOpacity,
-    View
+    View,
 } from 'react-native';
 
 export default function ChatScreen() {
@@ -36,7 +37,9 @@ What's the goal you're chasing right now?`,
     const [goalDescription, setGoalDescription] = useState<string | null>(null);
     const [error, setError] = useState<string | null>(null);
     const [finished, setFinished] = useState(false);
-    const {setEvents} = useEventsStore(); 
+    const { setEvents } = useEventsStore();
+    const { setGoals, goals } = useGoalsStore();
+
     const handleSendMessage = async () => {
         let firstGoalMessage = null;
         if (!goalDescription) {
@@ -75,27 +78,22 @@ Every step is built to push you forward. Check your calendar, lock in your focus
                         { content: finishedMessage, sender: 'bot' },
                     ]);
 
-                    // Persisting to AsyncStorage
+                    // Persisting to AsyncStorage / stores
                     try {
                         setEvents(response.events);
                         await AsyncStorage.setItem(
                             'ai_plan',
                             JSON.stringify(response.ai_plan)
                         );
-                        await AsyncStorage.setItem(
-                            'goal',
-                            JSON.stringify(response.goal)
-                        );
-                        setFinished(true)
+                        setGoals([...goals, response.goals]);
+                        setFinished(true);
                     } catch (error) {
                         console.error('AsyncStorage write failed:', error);
                     }
-                    //grab the events and add them to the session storage - need zustand persistence here.
                 }
-                console.log(response);
                 setLoading(false);
-                //  // go to index page, replacing login
             } else {
+                console.error(response.error);
                 setError(
                     'There was an error logging in. Please try again later.'
                 );
@@ -108,12 +106,6 @@ Every step is built to push you forward. Check your calendar, lock in your focus
 
     return (
         <ThemedView className=" pt-6 px-4 h-full relative">
-            <TouchableOpacity
-                className="bg-white/10 w-fit rounded-xl p-2 mb-4"
-                onPress={handleSendMessage}
-            >
-                <Text className="text-primary">SEND</Text>
-            </TouchableOpacity>
             <ScrollView className="space-y-4 pb-32">
                 {messages.map((message, index) => (
                     <MessageBubble key={index} message={message} />
@@ -122,7 +114,9 @@ Every step is built to push you forward. Check your calendar, lock in your focus
                 {loading && <TypingLoader />}
                 {finished && (
                     <PrimaryButton onPress={() => router.navigate('/')}>
-                        <Text className='font-satoshi text-center text-white font-bold text-lg '>Let's Go!</Text>
+                        <Text className="font-satoshi text-center text-white font-bold text-lg ">
+                            Let's Go!
+                        </Text>
                     </PrimaryButton>
                 )}
             </ScrollView>
@@ -139,12 +133,18 @@ Every step is built to push you forward. Check your calendar, lock in your focus
                     onSubmitEditing={handleSendMessage}
                     className="bg-white/10 text-white px-4 py-3 backdrop-blur-xl rounded-xl mb-4 w-11/12 pr-10"
                 />
-                <FontAwesome
+                <TouchableOpacity
+                    onPress={handleSendMessage}
+                    className="absolute right-12 top-12"
+                >
+                    <FontAwesome name="send" size={24} color="#FF6B00" />
+                </TouchableOpacity>
+                {/* <FontAwesome
                     name="microphone"
                     size={24}
                     color="#FF6B00"
                     className="absolute right-12 top-12"
-                />
+                /> */}
             </View>
         </ThemedView>
     );
