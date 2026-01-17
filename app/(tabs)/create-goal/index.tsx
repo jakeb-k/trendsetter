af671a5a009dc2fdc63cfc0c2d5cec066313415f
jakeb-k <jknow30@outlook.com>
2026-01-17 15:42:03 +1000

    chore: moved header code to own component

--- app/(tabs)/create-goal/index.tsx
+++ app/(tabs)/create-goal/index.tsx
@@ -1,31 +1,12 @@
-import TitleText from '@/components/common/TitleText';
+import CreateGoalHeader from '@/components/create-goal/CreateGoalHeader';
 import { ThemedView } from '@/components/ThemedView';
-import { Entypo } from '@expo/vector-icons';
-import { router } from 'expo-router';
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 
 export default function CreateGoalScreen() {
     return (
         <ScrollView className="flex-1 bg-secondary">
             <ThemedView className="min-h-screen w-full px-8 overflow-y-scroll">
-                <View className="flex flex-col w-full mt-6">
-                    <TouchableOpacity>
-                        <Entypo
-                            onPress={() => router.navigate('/calendar')}
-                            name="chevron-left"
-                            size={32}
-                            color="#FF6B00"
-                            className="-ml-2"
-                        />
-                    </TouchableOpacity>
-                    <TitleText
-                        className="font-bold text-2xl mt-4"
-                        title={'Set a Goal'}
-                    />
-                    <Text className="text-white/50 font-satoshi font-semibold my-1 tracking-wide">
-                        Make it real. No Vague Targets.
-                    </Text>
-                </View>
+                <CreateGoalHeader />
                 <View className="mt-2 h-[1px] w-full bg-gradient-to-r from-transparent via-[#FF7700]/70 to-transparent" />
             </ThemedView>
         </ScrollView>
---
+++ components/create-goal/CreateGoalHeader.tsx
@@ -0,0 +1,27 @@
+import { Entypo } from '@expo/vector-icons';
+import { router } from 'expo-router';
+import { Text, TouchableOpacity, View } from 'react-native';
+import TitleText from '../common/TitleText';
+
+export default function CreateGoalHeader() {
+    return (
+        <View className="flex flex-col w-full mt-6">
+            <TouchableOpacity>
+                <Entypo
+                    onPress={() => router.navigate('/calendar')}
+                    name="chevron-left"
+                    size={32}
+                    color="#FF6B00"
+                    className="-ml-2"
+                />
+            </TouchableOpacity>
+            <TitleText
+                className="font-bold text-2xl mt-4"
+                title={'Set a Goal'}
+            />
+            <Text className="text-white/50 font-satoshi font-semibold my-1 tracking-wide">
+                Make it real. No Vague Targets.
+            </Text>
+        </View>
+    );
+}
