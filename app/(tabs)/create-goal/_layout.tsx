85547a56b95dc15a400f595c9c14b5ca14026268
jakeb-k <jknow30@outlook.com>
2026-01-17 15:39:05 +1000

    ui: header for goals create page

--- app/(tabs)/create-goal/_layout.tsx
+++ app/(tabs)/create-goal/_layout.tsx
@@ -1,33 +1,9 @@
-import { Ionicons } from '@expo/vector-icons';
-import { router, Stack } from 'expo-router';
-import { TouchableOpacity } from 'react-native';
+import { Stack } from 'expo-router';
 
 export default function GoalCreateLayout() {
     return (
         <Stack>
-            <Stack.Screen
-                name="index"
-                options={{
-                    title: 'Set a New Goal!',
-                    headerTitleAlign: 'center', 
-                    headerStyle: { backgroundColor: '#FF6B00' },
-                    headerTitleStyle: {
-                        fontWeight: 'bold',
-                        fontFamily: 'Satoshi',
-                        color: '#fff',
-                        fontSize: 24,
-                    },
-                    headerLeft: () => (
-                        <TouchableOpacity onPress={() => router.back()}>
-                            <Ionicons
-                                name="chevron-back"
-                                size={24}
-                                color="white"
-                            />
-                        </TouchableOpacity>
-                    ),
-                }}
-            />
+            <Stack.Screen name="index" options={{ headerShown: false }} />
         </Stack>
     );
 }
--- app/(tabs)/create-goal/index.tsx
+++ app/(tabs)/create-goal/index.tsx
@@ -1,7 +1,33 @@
-export default function CreateGoal(){
+import TitleText from '@/components/common/TitleText';
+import { ThemedView } from '@/components/ThemedView';
+import { Entypo } from '@expo/vector-icons';
+import { router } from 'expo-router';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+
+export default function CreateGoalScreen() {
     return (
-        <>
-            penis
-        </>
-    )
+        <ScrollView className="flex-1 bg-secondary">
+            <ThemedView className="min-h-screen w-full px-8 overflow-y-scroll">
+                <View className="flex flex-col w-full mt-6">
+                    <TouchableOpacity>
+                        <Entypo
+                            onPress={() => router.navigate('/calendar')}
+                            name="chevron-left"
+                            size={32}
+                            color="#FF6B00"
+                            className="-ml-2"
+                        />
+                    </TouchableOpacity>
+                    <TitleText
+                        className="font-bold text-2xl mt-4"
+                        title={'Set a Goal'}
+                    />
+                    <Text className="text-white/50 font-satoshi font-semibold my-1 tracking-wide">
+                        Make it real. No Vague Targets.
+                    </Text>
+                </View>
+                <View className="mt-2 h-[1px] w-full bg-gradient-to-r from-transparent via-[#FF7700]/70 to-transparent" />
+            </ThemedView>
+        </ScrollView>
+    );
 }
